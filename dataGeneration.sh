4aeeb3a9468000a8ec77850bc3bab3e79c680083
Sim19 <Sim19@users.noreply.github.com>
2019-07-16 10:24:14 +0200

    Update dataGeneration.sh
    added folder creation and debugged folder-structure in code

--- dataGeneration.sh
+++ dataGeneration.sh
@@ -27,8 +27,13 @@ done
 
 R2METHOD=mean_V_mean_M
 
+## add data and data/stats/ folders if not existent
+if [! -d data/stats/]; then
+  	mkdir -p data/stats/
+fi
 
-FILENAME=../../data/stats/output_dataGeneration_C_${COND}_G_${GENES}_TF_${TF}_rep_${REP}_Sigma_${SIGMA}_FracNoise_${FRACNOISE}_Rsquared_${R2METHOD}_FRAC_${FRAC}.txt
+
+FILENAME=data/stats/output_dataGeneration_C_${COND}_G_${GENES}_TF_${TF}_rep_${REP}_Sigma_${SIGMA}_FracNoise_${FRACNOISE}_Rsquared_${R2METHOD}_FRAC_${FRAC}.txt
 echo Fit generated data with 
 echo 	${COND} conditions, 
 echo 	${GENES} genes, 
@@ -39,4 +44,4 @@ echo 	${FRAC}% variation explained.
 echo 	${FRACNOISE}% variation in noise.
 
 # python command:
-time -p python ../01_simulation/datageneration/generate_data.py -c ${COND} -G ${GENES} -T ${TF} -r ${REP} -S ${SIGMA} -R ${R2METHOD} -f ${FRAC} --fN ${FRACNOISE} |&tee -a ${FILENAME}
+time -p python generate_data.py -c ${COND} -G ${GENES} -T ${TF} -r ${REP} -S ${SIGMA} -R ${R2METHOD} -f ${FRAC} --fN ${FRACNOISE} |&tee -a ${FILENAME}
